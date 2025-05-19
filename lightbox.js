26e61e7a783ee45c12b7f49eec318469fd10f88c
KabeloMaps <mpnkab004@myuct.ac.za>
2025-05-19 20:27:01 +0200

    Lightbox Image Gallery

---
+++ lightbox.js
@@ -0,0 +1,10 @@
+function openLightbox(src) {
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+  lightbox.style.display = "flex";
+  lightboxImg.src = src;
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
