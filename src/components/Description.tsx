be4f16d7101bb98af0fbc98a18e93427ccd964c9
KabeloMaps <mpnkab004@myuct.ac.za>
2025-12-19 19:49:21 +0200

    Changed the letter spacing

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Description = () => {
   return (
-    <div className="bg-green-400">
-      <h1 className="font-bold text-[1rem]">SNEAKER COMPANY</h1>
-      <h2>Fall Limited Edition Sneakers</h2>
-      <p className="text-[1rem] font-">
+    <div className=" py-6 px-6">
+      <h1 className="font-bold text-[1.55rem] text-gray-400 tracking-[0.22rem]">
+        SNEAKER COMPANY
+      </h1>
+      <h2 className="text-[2.85rem] font-bold tracking-[0.1rem] pb-6">
+        Fall Limited Edition Sneakers
+      </h2>
+      <p className="text-[1.72rem] font-[secondary] ">
         These low-profile sneakers are your perfect casual wear companion.
         Featuring a durable rubber outer sole, they’ll withstand everything the
         weather can offer.
